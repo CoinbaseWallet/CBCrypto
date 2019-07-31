96f174283d042ca78161e0edf86701a5fa8047bc
HenryNguyen5 <ngu.henry@hotmail.com>
2019-07-31 16:25:35 -0500

    Add android module with sha256 string extension (#2)
    * Add android module with sha256 string extension

---
+++ android/crypto/src/main/java/com/coinbase/wallet/crypto/extensions/ByteArray+Crypto.kt
@@ -0,0 +1,16 @@
+package com.coinbase.wallet.crypto.extensions
+
+/**
+ * Convert ByteArray to hex encoded string
+ */
+fun ByteArray.toHexString(): String {
+    val result = StringBuffer()
+    for (byt in this) {
+        val hex = Integer.toString((byt and 0xff) + 0x100, 16).substring(1)
+        result.append(hex)
+    }
+
+    return result.toString()
+}
+
+private infix fun Byte.and(value: Int) = toInt() and value
