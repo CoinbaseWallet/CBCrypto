22d22dd7435bacd453041d3a9f3edd39a1070fb5
Tuấn Ngô <ngo461596@gmail.com>
2021-11-30 11:46:30 +0000

    Revert "Add crypto lock (#4)"
    This reverts commit 9b0990148600a7eb9b996b7f060b76761f6792ce.

--- android/crypto/src/main/java/com/coinbase/wallet/crypto/ciphers/CipherLock.kt
+++
@@ -1,8 +0,0 @@
-package com.coinbase.wallet.crypto.ciphers
-
-import java.util.concurrent.locks.ReentrantLock
-
-/**
- * A lock for crypto module to use to synchronize crypto operations
- */
-internal object CipherLock : ReentrantLock()
